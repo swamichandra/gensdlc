560d0c52a55e04401507a3b2b418fe3c789656e0
Swami Chandrasekaran <swamichandra@gmail.com>
2023-06-29 23:04:46 -0500

    Create css.py

---
+++ css.py
@@ -0,0 +1,20 @@
+v1 = """
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed&display=swap');
+
+html, body, [class*="css"] {
+    font-family: 'IBM Plex Sans Condensed', sans-serif;
+}
+
+div.stButton > button {
+        cursor:pointer;outline:0;display:inline-block;font-weight:400;line-height:1.5;text-align:center;background-color:transparent;border:2px solid transparent;padding:6px 12px;font-size:1rem;border-radius:.25rem;transition:color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;color:#bf470a;border-color:#bf470a; display:block; margin: 0 auto;        
+}
+
+div.stButton > button:hover {
+    color:#fff;background-color:#bf470a;border-color:#bf470a
+}
+
+.small-font {
+    font-size: 8px !important;
+}
+
+"""
