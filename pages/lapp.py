9d4151d21d77ba3bfdf9da85d3b7ea6d7876d65e
Swami Chandrasekaran <swamichandra@gmail.com>
2023-07-19 12:55:27 -0500

    Update lapp.py

--- pages/lapp.py
+++ pages/lapp.py
@@ -14,10 +14,17 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.llms import VertexAI
 from langchain import PromptTemplate, LLMChain
 from google.cloud import aiplatform
+from google.oauth2 import service_account
 from vertexai.preview.language_models import TextGenerationModel
 
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+
 PROJECT_ID = "learning-351419"  # @param {type:"string"}
-vertexai.init(project=PROJECT_ID, location="us-central1")
+vertexai.init(project=PROJECT_ID, location="us-central1", credentials=credentials)
+
 
 def generate_response(txt):
     PROJECT_ID = "learning-351419"  # @param {type:"string"}
@@ -56,28 +63,19 @@ st.set_page_config(page_title="Generative AI Text Summarization App",
 st.title('📚 Generative AI Text Summarization App')
 
 # Create a file upload widget for the credentials JSON file
-creds_file = st.file_uploader(
-    "Upload Google Cloud credentials file", type="json")
+#creds_file = st.file_uploader(
+#    "Upload Google Cloud credentials file", type="json")
 
-if creds_file is not None:
-    now = datetime.now()
-    randomfilename = "temp_credentials_" + now.strftime("%m%d%Y_%H%M%S")
+# Text input
+txt_input = st.text_area('Enter your text to summarize', 'Function to generate prime numbers', height=200)
 
-    creds_contents = creds_file.read().decode("utf-8")
-    with open(randomfilename, "w") as f:
-        f.write(creds_contents)
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = randomfilename
+result = []
+if st.button("Submit"):
+    with st.spinner('Performing magic ...'):
+        st.info(txt_input)
+        response = generate_response(txt_input.strip())
+        result.append(response)
 
-    # Text input
-    txt_input = st.text_area('Enter your text to summarize', 'Function to generate prime numbers', height=200)
-
-    result = []
-    if st.button("Submit"):
-        with st.spinner('Performing magic ...'):
-            st.info(txt_input)
-            response = generate_response(txt_input.strip())
-            result.append(response)
-
-    # Display result
-    if len(result):
-        st.write(response)
+# Display result
+if len(result):
+    st.write(response)
