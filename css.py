8dc903f725152168861187539df7b9c510e3ee9f
Swami Chandrasekaran <swamichandra@gmail.com>
2023-07-20 10:14:26 -0500

    Update css.py

--- css.py
+++ css.py
@@ -6,7 +6,7 @@ html, body, [class*="css"] {
 }
 
 div.stButton > button {
-        cursor:pointer;outline:0;display:inline-block;font-weight:400;line-height:1.5;text-align:center;background-color:transparent;border:2px solid transparent;padding:6px 12px;font-size:1rem;border-radius:.25rem;transition:color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;color:#000;background-color:#64b5f6;border-color:#64b5f6; display:block; margin: 0 auto; font-weight : bold;       
+        cursor:pointer;outline:0;display:inline-block;font-weight:400;line-height:1.5;text-align:center;background-color:transparent;border:2px solid transparent;padding:6px 12px;font-size:1rem;border-radius:.25rem;transition:color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;color:#000;background-color:#64b5f6;border-color:#64b5f6; display:block; margin: 0 auto;       
 }
 
 div.stButton > button:hover {
@@ -15,6 +15,7 @@ div.stButton > button:hover {
 
 .small-font {
     font-size: 9px !important;
+    font-weight : bold;
 }
 
 """
