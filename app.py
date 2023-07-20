c13ca83e6ec4694de451ad46f996d0c3490f4afc
Swami Chandrasekaran <swamichandra@gmail.com>
2023-07-19 23:01:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -281,8 +281,9 @@ if submit_button:
         my_bar = st.progress(0, text=progress_text)
 
         for percent_complete in range(100):
-            time.sleep(0.05)
+            time.sleep(0.01)
             my_bar.progress(percent_complete + 1, text=progress_text)
+        my_bar.empty()
         
         #time.sleep(10)
         with tab1:
