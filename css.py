1c6565722a48a23750421047ff5fd772167eb498
Swami Chandrasekaran <swamichandra@gmail.com>
2023-07-20 09:53:29 -0500

    Update css.py

--- css.py
+++ css.py
@@ -6,7 +6,7 @@ html, body, [class*="css"] {
 }
 
 div.stButton > button {
-        cursor:pointer;outline:0;display:inline-block;font-weight:400;line-height:1.5;text-align:center;background-color:transparent;border:2px solid transparent;padding:6px 12px;font-size:1rem;border-radius:.25rem;transition:color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;color:#fff;background-color:#90caf9;border-color:#90caf9; display:block; margin: 0 auto;        
+        cursor:pointer;outline:0;display:inline-block;font-weight:400;line-height:1.5;text-align:center;background-color:transparent;border:2px solid transparent;padding:6px 12px;font-size:1rem;border-radius:.25rem;transition:color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;color:#555;background-color:#90caf9;border-color:#90caf9; display:block; margin: 0 auto;        
 }
 
 div.stButton > button:hover {
